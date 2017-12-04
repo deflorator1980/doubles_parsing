c4b11e4da9614f1da973e61aa1c1b1338d89799c
Andrey Isakov <a.isakov@nelant.ru>
2017-12-04 15:16:00 +0300

    vi conditions

---
+++ BinarySearch.java
@@ -0,0 +1,32 @@
+class BinarySearch {
+    int binarySearch(double arr[], int l, int r, int x) {
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+            if ((int) arr[mid] == mid)
+                return mid;
+
+            if ((int) arr[mid] < mid)
+                return binarySearch(arr, l, mid - 1, mid -1);
+
+
+            return binarySearch(arr, mid + 1, r, mid +1);
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        BinarySearch ob = new BinarySearch();
+//        double arr[] = {1.2, 2.4, 3.11, 3.14, 10.14, 20.14};
+        double arr[] = {1.2, 2.4, 3.11, 3.14, 3.44, 5.14};
+//        double arr[] = {1.2, 2.4, 3.11, 4.14, 5.04, 5.14};
+//        int arr[] = {-2, -1, 0 , 3, 10, 20};
+        int n = arr.length;
+        int x = 3;
+        int result = ob.binarySearch(arr, 0, n - 1, x);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
